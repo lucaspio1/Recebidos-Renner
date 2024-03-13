c17d5748a18a6011f5f62df8f249f6bdfde7d14e
Lucas Pio <lucaspioh@gmail.com>
2024-03-12 22:33:42 -0300

    a

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import routes from './routes.js';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +15,9 @@ app.use(express.static(__dirname));
 
 // Permite o parse de dados de formulário
 app.use(express.urlencoded({ extended: true }));
+
+// Conexão com o MongoDB usando Mongoose
+mongoose.connect('mongodb+srv://lucaspioh:jPA3tDqNxbyqwtzl@cluster0.tlvz0ze.mongodb.net/TesteRecebimentoRenner?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Importa as rotas definidas em routes.js
 app.use('/', routes);
