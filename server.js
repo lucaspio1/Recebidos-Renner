dd763b2b8b773bbd1fc09661a0744f1666c8f733
Lucas Pio <lucaspioh@gmail.com>
2024-03-12 23:10:12 -0300

    funcionando 100%

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
 import routes from './routes.js';
-import mongoose from 'mongoose';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,26 +16,32 @@ app.use(express.static(__dirname));
 // Permite o parse de dados de formulário
 app.use(express.urlencoded({ extended: true }));
 
-// Define a função de conexão com o banco de dados
-const connectDB = async () => {
+// Importa as rotas definidas em routes.js
+app.use('/', routes);
+
+// URL de conexão com seu banco de dados MongoDB
+const url = 'mongodb+srv://lucaspioh:jPA3tDqNxbyqwtzl@cluster0.tlvz0ze.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Nome do banco de dados
+const dbName = 'TesteRecebimentoRenner';
+
+async function startServer() {
   try {
-    await mongoose.connect('mongodb+srv://lucaspioh:jPA3tDqNxbyqwtzl@cluster0.tlvz0ze.mongodb.net/TesteRecebimentoRenner?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('Conexão MongoDB estabelecida com sucesso');
-    // Inicie o servidor após a conexão bem-sucedida
+    console.log('Conectando ao MongoDB...');
+    const client = new MongoClient(url);
+    await client.connect();
+    console.log('Conexão com o MongoDB estabelecida com sucesso');
+
+    // Inicia o servidor após a conexão ser estabelecida
     app.listen(port, () => {
       console.log(`Servidor iniciado na porta ${port}`);
     });
   } catch (error) {
     console.error('Erro ao conectar ao MongoDB:', error);
-    process.exit(1); // Encerre o processo em caso de erro de conexão
+    process.exit(1); // Encerra o processo se a conexão falhar
   }
-};
+}
 
-// Chame a função de conexão com o banco de dados antes de definir as rotas
-connectDB();
+// Inicia o servidor
+startServer();
 
-// Importa as rotas definidas em routes.js
-app.use('/', routes);
