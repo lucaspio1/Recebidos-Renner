7d63d70d21d62538cbd195780104a2da22e84586
Lucas Pio <lucaspioh@gmail.com>
2024-03-12 22:39:42 -0300

    c

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { dirname } from 'path';
 import routes from './routes.js';
 import mongoose from 'mongoose';
 
@@ -16,12 +16,26 @@ app.use(express.static(__dirname));
 // Permite o parse de dados de formulário
 app.use(express.urlencoded({ extended: true }));
 
-// Conexão com o MongoDB usando Mongoose
-mongoose.connect('mongodb+srv://lucaspioh:jPA3tDqNxbyqwtzl@cluster0.tlvz0ze.mongodb.net/TesteRecebimentoRenner?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+// Define a função de conexão com o banco de dados
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://lucaspioh:jPA3tDqNxbyqwtzl@cluster0.tlvz0ze.mongodb.net/TesteRecebimentoRenner?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Conexão MongoDB estabelecida com sucesso');
+    // Inicie o servidor após a conexão bem-sucedida
+    app.listen(port, () => {
+      console.log(`Servidor iniciado na porta ${port}`);
+    });
+  } catch (error) {
+    console.error('Erro ao conectar ao MongoDB:', error);
+    process.exit(1); // Encerre o processo em caso de erro de conexão
+  }
+};
+
+// Chame a função de conexão com o banco de dados antes de definir as rotas
+connectDB();
 
 // Importa as rotas definidas em routes.js
 app.use('/', routes);
-
-app.listen(port, () => {
-  console.log(`Servidor iniciado na porta ${port}`);
-});
